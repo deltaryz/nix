11cd8ce66207dc4e6ede00ff23a736760f5d98e8
techniponi <wincam97@gmail.com>
2020-03-18 05:32:34 -0500

    add workstation configuration

---
+++ dev/workstation/configuration.nix
@@ -0,0 +1,19 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+
+  networking.hostName = "workstation"; # Define your hostname.
+  networking.interfaces.wlp3s0.useDHCP = true;
+  networking.interfaces.enp2s0.useDHCP = true;
+
+  imports =
+  [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../default.nix
+  ];
+
+}
