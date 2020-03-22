bbbb25311bf040ca9c33c96e496c96c7495f4d16
techniponi <wincam97@gmail.com>
2020-03-21 20:36:14 -0500

    aaaaaaaa

--- default.nix
+++ default.nix
@@ -44,7 +44,8 @@
   virtualisation.docker.enable = true;
 
   services.openssh.enable = true;
-  users.users.delta.openssh.keyFiles = ["./secret/authorized_keys"];
+  users.users.delta.openssh.authorizedKeys = ["./secret/authorized_keys"];
+  users.users.delta.keyFiles = ["./secret/authorized_keys"];
   services.openssh.authorizedKeysFiles = ["./secret/authorized_keys"];
   services.openssh.extraConfig =
   ''
